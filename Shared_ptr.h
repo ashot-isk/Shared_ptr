e2899ad28f3dddc2f813b3648f2f03065427ca17
Ashot-Is <78801215+Ashot0101@users.noreply.github.com>
2021-08-20 21:15:25 +0400

    Update Shared_ptr.h

--- Shared_ptr.h
+++ Shared_ptr.h
@@ -8,28 +8,29 @@ private:
 	struct ControlBlock
 	{
 		int RC;
+
 		std::function<void(T *)> deleter;
 	};
 	T *ptr;
 	ControlBlock *controlBlock;
 
 public:
-	int get()
+	int use_count()
 	{
 		return this->controlBlock->RC;
 	}
 	Shared_ptr();
 	Shared_ptr(T *);
-	Shared_ptr(T* ,std::function<void(T *)> deleter );
+	Shared_ptr(T *, std::function<void(T *)> deleter);
 	Shared_ptr(const Shared_ptr &ctor); //copy constructor
 	Shared_ptr(Shared_ptr &&tmp);	    // muve constructor
 	Shared_ptr &operator=(const Shared_ptr &obj);
 	Shared_ptr &operator=(Shared_ptr &&tmp); //muve assignment
-	T& operator*() const
+	T &operator*() const
 	{
 		return *ptr;
 	}
-	T* operator->() const
+	T *operator->() const
 	{
 		return ptr;
 	}
@@ -74,24 +75,30 @@ Shared_ptr<T>::Shared_ptr(const Shared_ptr<T> &other)
 	this->controlBlock->RC++;
 }
 
-/**************muve constructor**********/
+/**************move constructor**********/
 template <typename T>
 Shared_ptr<T>::Shared_ptr(Shared_ptr<T> &&tmp)
 {
-	//this->controlBlock = new ControlBlock();
+	
 	this->controlBlock = tmp.controlBlock;
 	this->ptr = tmp.ptr;
-	tmp.controlBlock = nullptr;
+	
+	tmp.controlBlock = new ControlBlock();
 	tmp.ptr = nullptr;
 }
 
-/**************muve assignment**********/
+/**************move assignment**********/
 template <typename T>
 Shared_ptr<T> &Shared_ptr<T>::operator=(Shared_ptr<T> &&tmp)
-{
+{   
+	if(this == &tmp)
+	{	
+		return *this;
+	}
+      
 	this->controlBlock = tmp.controlBlock;
 	this->ptr = tmp.ptr;
-	tmp.controlBlock = nullptr;
+	tmp.controlBlock = new ControlBlock();
 	tmp.ptr = nullptr;
 	return *this;
 }
@@ -106,7 +113,20 @@ Shared_ptr<T> &Shared_ptr<T>::operator=(const Shared_ptr<T> &other)
 	}
 	if (this->ptr)
 	{
+		
 		this->controlBlock->RC--;
+		if (controlBlock->RC == 0)
+		{
+			delete controlBlock;
+			controlBlock = nullptr;
+		}
+		if (!other.ptr)
+		{
+			this->controlBlock = other.controlBlock;
+			this->ptr = other.ptr;
+			return *this;
+		}
+
 		this->controlBlock = other.controlBlock;
 		this->ptr = other.ptr;
 		this->controlBlock->RC++;
@@ -118,4 +138,14 @@ Shared_ptr<T> &Shared_ptr<T>::operator=(const Shared_ptr<T> &other)
 		this->controlBlock->RC++;
 	}
 	return *this;
+}
+
+template <typename T>
+
+Shared_ptr<T>::Shared_ptr(T *ptr, std::function<void(T *)> deleter)
+{
+	this->ptr = ptr;
+	controlBlock = new ControlBlock();
+	controlBlock->deleter = deleter;
+	controlBlock->RC = 1;
 }
